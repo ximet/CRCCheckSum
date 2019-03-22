a781a5451f122a70271aca4cc0aa3db4a923beee
DmitryBardt <dq.ximet@gmail.com>
2019-03-22 18:28:02 +0300

    add common crc algorithm

--- crc.py
+++ crc.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-def crc8_oneByte(data):
-    polynom = 0x1D;
-    crc = data;
+
+def crc8_common(crc, polynom):
     for i in range (8):
         if (int(crc, 2) & 0x80):
             crc = bin((int(crc, 2) << 1) ^ polynom);
-            print("With <<", crc)
+            print("With <<", crc);
         else:
             crc = bin(int(crc, 2) << 1);
-            print("Without <<", crc)
+            print("Without <<", crc);
+    return crc;
+
+def crc8_oneByte(data):
+    polynom = 0x1D;
+    crc = crc8_common(data, polynom);
 
     return crc[-8:]
 
