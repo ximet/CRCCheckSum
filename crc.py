d2511d9214c18d35683196efda4394b55621946e
DmitryBardt <dq.ximet@gmail.com>
2019-03-22 14:55:42 +0300

    fix problem with crc bin format

--- crc.py
+++ crc.py
@@ -3,11 +3,13 @@ def crc(data):
     polynom = 0x1D;
     crc = data;
     for i in range (8):
-        if (crc & 0x80):
-            crc = ((crc << 1) ^ polynom);
+        if (int(crc, 2) & 0x80):
+            crc = bin((int(crc, 2) << 1) ^ polynom);
+            print("With <<", crc)
         else:
-            crc <<= 1;
-    return bin(crc)
+            crc = bin(int(crc, 2) << 1);
+            print("Without <<", crc)
 
+    return crc[-8:]
 
-print(crc(0xC2))
+print(crc("0b11000010"))
