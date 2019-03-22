79e3838ec2bd93e252fa2bf43143d561df62117e
DmitryBardt <dq.ximet@gmail.com>
2019-03-22 19:07:50 +0300

    add binary view to crc

--- crc.py
+++ crc.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
 def crc8_common(crc, polynom):
-    print("Start:", crc)
     for i in range (8):
         if (int(crc, 2) & 0x80):
             crc = bin((int(crc, 2) << 1) ^ polynom)
-            print("With <<", crc)
         else:
             crc = bin(int(crc, 2) << 1)
-            print("Without <<", crc)
     return crc
 
 def crc8_oneByte(data):
@@ -24,6 +21,6 @@ def crc8(bytearray):
         crc ^= int(currentByte, 2)
         crc8_common(bin(crc), polynom)
 
-    return crc
+    return bin(crc)[-8:]
 
 print(crc8(["0b00000001", "0b00000010"]))
