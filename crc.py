eefbf06dc5e4cae1868b83dfa6119b2eb9cf3a64
DmitryBardt <dq.ximet@gmail.com>
2019-04-02 17:47:57 +0300

    CRC-8 byte-by-byte algorithm

--- crc.py
+++ crc.py
@@ -4,6 +4,7 @@ def crc8_common(crc, polynom):
     for i in range (8):
         if (int(crc, 2) & 0x80):
             crc = bin((int(crc, 2) << 1) ^ polynom)
+
         else:
             crc = bin(int(crc, 2) << 1)
     return crc
@@ -18,9 +19,9 @@ def crc8(bytearray):
     polynom = 0x1D
     crc = 0
     for currentByte in bytearray:
-        crc ^= int(currentByte, 2)
-        crc8_common(bin(crc), polynom)
+        crc = crc ^ currentByte
+        crc = int(crc8_common(bin(crc), polynom), 2)
 
     return bin(crc)[-8:]
 
-print(crc8(["0b00000001", "0b00000010"]))
+print(crc8(bytearray([1, 2])))
